96b8f4aad804bccb95c4d7cfbeb517b45490b4f1
Lee Kol <lee.levy333@gmail.com>
2022-05-14 16:31:49 +0300

    Task 2 is done.

---
+++ task_2.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+"""
+The program creates a vector of length 10 with values evenly distributed between 5 and 50.
+"""
+
+if __name__ == '__main__':
+    vector = np.linspace(5, 50, 10)
+    print(vector)
